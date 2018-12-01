749c8e0fbbd5069b3c4665c321b9d0c5a589b0a4
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-11-30 22:41:30 -0600

    created Persistence Interface, XmlPersistence and Abrastract Model Base

---
+++ Project0/Project0.Library/Control/Persistence/XmlPersistence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Project0.Library.Model;
+
+namespace Project0.Library.Control.Persistence
+{
+    public class XmlPersistence : IPersistence
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AModelBase Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AModelBase Find(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Store()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Store(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project0/Project0.Library/Model/AModelBase.cs
@@ -0,0 +1,14 @@
+﻿using Project0.Library.Control.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library.Model
+{
+    public abstract class AModelBase : XmlPersistence
+    {
+        protected readonly string _modelName;
+
+        protected abstract void setModelName(string modelName);
+    }
+}
