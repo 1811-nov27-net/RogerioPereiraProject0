6c8ba7600c66c3d0c082cad969672a9204024dd7
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-11-30 23:42:36 -0600

    Order Model

---
+++ Project0/Project0.Library/Model/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library.Model
+{
+    /// <summary>
+    /// Ingrendients Model
+    /// </summary>
+    public class Order : AModelBase
+    {
+        /// <summary>
+        /// AModelBase function
+        /// </summary>
+        /// <returns>Model Name</returns>
+        protected override string GetModelName()
+        {
+            return "Order";
+        }
+
+        public int Id { get; set; }
+        public int CostumerId { get; set; }
+        public int AddressId { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public double Value { get; set; }
+        public DateTime date { get; set; }
+    }
+}
