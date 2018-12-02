07ea665a127648a76f75097489bc5154fb757242
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-01 18:51:01 -0600

    Test Add Pizza to Order

---
+++ Project0/Project0.Tests/Library/Model/OrderTest.cs
@@ -0,0 +1,65 @@
+﻿using Project0.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Project0.Tests.Library.Model
+{
+    public class OrderTest
+    {
+        [Fact]
+        public void CanAdd12Pizzas()
+        {
+            Order o = new Order();
+
+            for (int i=0; i<12; i++)
+            {
+                Pizza p = new Pizza();
+                p.Id = 1;
+                p.Name = "Pizza";
+                p.Price = 10;
+
+                o.AddPizza(p);
+            }
+
+            Assert.Equal(12, o.Pizzas.Count);
+        }
+
+        [Fact]
+        public void CannotAdd13Pizzas()
+        {
+            Order o = new Order();
+
+            for (int i=0; i<13; i++)
+            {
+                Pizza p = new Pizza();
+                p.Id = 1;
+                p.Name = "Pizza";
+                p.Price = 10;
+
+                o.AddPizza(p);
+            }
+
+            Assert.Equal(12, o.Pizzas.Count);
+        }
+
+        [Fact]
+        public void ValueCannotBeMoreThan500()
+        {
+            Order o = new Order();
+
+            for (int i=0; i<12; i++)
+            {
+                Pizza p = new Pizza();
+                p.Id = 1;
+                p.Name = "Pizza";
+                p.Price = 50;
+
+                o.AddPizza(p);
+            }
+
+            Assert.Equal(10, o.Pizzas.Count);
+        }
+    }
+}
