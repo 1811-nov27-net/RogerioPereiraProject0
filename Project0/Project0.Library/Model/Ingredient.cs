4be01c8010c1f94ccb1830b4df0ec69e7dc351eb
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-03 10:53:39 -0600

    Constructor for Ingredient Model

--- Project0/Project0.Library/Model/Ingredient.cs
+++ Project0/Project0.Library/Model/Ingredient.cs
@@ -18,6 +18,13 @@ namespace Project0.Library.Model
             return "Ingredient";
         }
 
+        public Ingredient(int id, string name, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+;        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
