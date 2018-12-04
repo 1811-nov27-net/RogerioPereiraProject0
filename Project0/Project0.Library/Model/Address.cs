43412b3979fd9fa460573201df266beeda62eda5
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-04 15:12:00 -0600

    added Street2 field

--- Project0/Project0.Library/Model/Address.cs
+++ Project0/Project0.Library/Model/Address.cs
@@ -21,6 +21,7 @@ namespace Project0.Library.Model
         public int Id { get; set; }
         public int CostumerId { get; set; }
         public string Street { get; set; }
+        public string Street2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int ZipCode { get; set; }
