b85fb4311abfe10729bc3636e33a6fde555b5b76
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-05 13:23:54 -0600

    Change Address model: street 1 and 2 to address

--- Project0/Project0.Interface/View/Costumers/CostumerForm.cs
+++ Project0/Project0.Interface/View/Costumers/CostumerForm.cs
@@ -17,8 +17,11 @@ namespace Project0.Interface.View.Costumers
             Console.Write("Cosutmer Last Name:\n");
             costumer.LastName = Console.ReadLine();
 
-            Console.Write("Street:\n");
-            costumer.Address.Street = Console.ReadLine();
+            Console.Write("Address Line 1:\n");
+            costumer.Address.Address1 = Console.ReadLine();
+
+            Console.Write("Address Line 2:\n");
+            costumer.Address.Address2 = Console.ReadLine();
 
             Console.Write("City:\n");
             costumer.Address.City = Console.ReadLine();
--- Project0/Project0.Library/Model/Address.cs
+++ Project0/Project0.Library/Model/Address.cs
@@ -20,8 +20,8 @@ namespace Project0.Library.Model
 
         public int Id { get; set; }
         public int CostumerId { get; set; }
-        public string Street { get; set; }
-        public string Street2 { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int ZipCode { get; set; }
