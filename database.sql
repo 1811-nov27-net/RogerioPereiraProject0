07a3971431fcd04b157f15a94ec0fcf69920e515
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-06 10:41:15 -0600

    Seeds

--- database.sql
+++ database.sql
@@ -47,7 +47,7 @@ create table pizza.addresses (
 	id int IDENTITY(1,1) NOT NULL,
 	customerId int not null,
 	address1 nvarchar(100) not null,
-	address2 nvarchar(100) not null,
+	address2 nvarchar(100) null,
 	city nvarchar(100) not null,
 	state nchar(2) not null,
 	zipcode int not null
@@ -112,3 +112,84 @@ alter table pizza.orderPizzas
 	add constraint FK_OrderPizza_Pizza foreign key (pizzaId) references pizza.pizzas (id)
 		on delete no action
 		on update cascade;
+
+--Seeds
+--Ingredients
+insert into pizza.ingredients (name, stock) values
+('Dough', 10),				--1
+('Tomato Sauce', 10),		--2
+('Mozzarela', 10),			--3
+('Provolone', 10),			--4
+('Cheddar', 10),			--5
+('Ham', 10),				--6
+('Bacon', 10),				--7
+('Canadian Bacon', 10),		--8
+('Tomato', 10),				--9
+('Olive', 10),				--10
+('Egg', 10),				--11
+('Pea', 10),				--12 --ervilha
+('basil', 10);				--13 --manjeiricão
+
+--Pizzas
+insert into pizza.pizzas (name, price) values
+('Ham and Cheese', 20),	--1
+('Canadian Bacon', 40),	--2
+('Bacon', 40),			--3
+('Protuguese', 50),		--4
+('Margherita', 30);		--5
+
+--PizzasIngredients
+insert into pizza.pizzasIngredients (pizzaId, ingredientId) values
+-- Ham and Cheese (dought, Tomato Sauce, Mozzarela, Ham
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 6),
+-- Canadian Bacon (dought, Tomato Sauce, Mozzarela, Canadian Bacon)
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 8),
+-- Bacon (dought, Tomato Sauce, Mozzarela, Bacon)
+(3, 1),
+(3, 2),
+(3, 3),
+(3, 7),
+-- Portuguese (dought, Tomato Sauce, Mozzarela, Ham, Egg, Pea)
+(4, 1),
+(4, 2),
+(4, 3),
+(4, 6),
+(4, 11),
+(4, 12),
+-- Margherita (dought, Tomato Sauce, Mozzarela, Tomato, Basil)
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 9),
+(5, 13);
+
+--Customers
+insert into pizza.customers (firstName, lastName) values
+('Rogerio', 'Pereira'), --1
+('Customer', '1'),		--2
+('Customer', '2'),		--3
+('Customer', '3');		--4
+
+insert into pizza.addresses (customerId, address1, city, state, zipcode) values
+(1, '332 North Croft Ave', 'Los Angeles', 'CA', 90048),
+(2, '2 Address Customer 2', 'City', 'ST', 12345),
+(3, '3 Address Customer 3', 'City', 'ST', 45678),
+(4, '4 Address Customer 4', 'City', 'ST', 98765);
+
+insert into pizza.orders (customerId, addressId, value, date) values
+(1, 1, 40, '2018-12-06 10:27:00'),	--1
+(2, 2, 40, '2018-12-06 10:27:00'),	--2
+(3, 3, 40, '2018-12-06 10:27:00'),	--3
+(4, 4, 40, '2018-12-06 10:27:00');	--4
+
+insert into pizza.orderPizzas (orderId, pizzaId) values
+(1, 3),
+(2, 3),
+(3, 3),
+(4, 3);
