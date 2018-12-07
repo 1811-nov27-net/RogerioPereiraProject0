6828666425bb8e1ef26f460b5f962afa4ae0ec25
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-06 20:32:50 -0600

    Repository interface and Abstract Class

--- Project0/DataAccess/AModel.cs
+++ Project0/DataAccess/AModel.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Project0.DataAccess
 {
+    /// <summary>
+    /// Abstract Class to be used in IRepository
+    /// </summary>
     public abstract class AModel
     {
     }
---
+++ Project0/DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.DataAccess.Repositories
+{
+    /// <summary>
+    /// Repository's Interface
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Get all data stored in data base
+        /// </summary>
+        /// <returns>IList (AModel)</returns>
+        IList<AModel> GetAll();
+
+        /// <summary>
+        /// Get model based on ID
+        /// </summary>
+        /// <param name="id">Model's id</param>
+        /// <returns>AModel</returns>
+        AModel GetById(int id);
+
+        /// <summary>
+        /// Get model based on name
+        /// </summary>
+        /// <param name="name">Model's name</param>
+        /// <returns>AModel</returns>
+        AModel GetByName(string name);
+
+        /// <summary>
+        /// Save a model
+        /// </summary>
+        /// <param name="model">Model to be saved</param>
+        /// <param name="id">Model's id</param>
+        /// <returns>Model saved</returns>
+        AModel Save(AModel model, int? id = null);
+
+        /// <summary>
+        /// Delete model
+        /// </summary>
+        /// <param name="model">Model to be deleted</param>
+        /// <returns>Operation's Status</returns>
+        bool Delete(AModel model);
+    }
+}
