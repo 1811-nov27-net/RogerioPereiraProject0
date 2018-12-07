e63c25efd626ed7d856f8d5eb2538ddacdf6d3fd
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-07 10:11:22 -0600

    Starting DataBaseConfiguration on Program.cs

--- Project0/Project0.Interface/Program.cs
+++ Project0/Project0.Interface/Program.cs
@@ -1,13 +1,27 @@
-﻿using Project0.Interface.View.Menu;
+﻿using Microsoft.EntityFrameworkCore;
+using Project0.DataAccess;
+using Project0.Interface.View.Menu;
 using System;
 
 namespace Project0.Interface
 {
     public class Program
     {
+        static DbContextOptions<Project0Context> options = null;
+
         static void Main(string[] args)
         {
+            ConfigDatabase();
             MainMenu.ShowMainMenu();
+        }
+
+        private static void ConfigDatabase()
+        {
+            var connectionString = SecretDatabaseAccess.SecretString;
+
+            var optionsBuilder = new DbContextOptionsBuilder<Project0Context>();
+            optionsBuilder.UseSqlServer(connectionString);
+            options = optionsBuilder.Options;
         }
     }
 }
